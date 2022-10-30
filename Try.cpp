475d75cb913ba8eac1c2d47a0f8455078ce10bd9
Kamalesh P <66113370+kamalesh-p@users.noreply.github.com>
2022-10-30 19:39:35 +0530

    Create Try.cpp

---
+++ Try.cpp
@@ -0,0 +1,7 @@
+/*
+https://www.pythonanywhere.com/user/dummytestnewproject/files/home/dummytestnewproject/DjangoWebProject/DjangoWebProject1/app1
+https://www.pythonanywhere.com/user/dummytestnewproject/files/home/dummytestnewproject/DjangoWebProject/DjangoWebProject1/app1/views.py?edit
+https://www.pythonanywhere.com/user/dummytestnewproject/files/home/dummytestnewproject/DjangoWebProject/DjangoWebProject1/app1/models.py?edit
+https://www.pythonanywhere.com/user/dummytestnewproject/files/home/dummytestnewproject/DjangoWebProject/DjangoWebProject1/app1/urls.py?edit
+
+*/
