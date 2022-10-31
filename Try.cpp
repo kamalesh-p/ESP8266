0390b3c7f0e72b01c3e908aa1c3c3c20b7a33104
Kamalesh P <66113370+kamalesh-p@users.noreply.github.com>
2022-10-31 09:38:37 +0530

    Delete Try.cpp

--- Try.cpp
+++
@@ -1,7 +0,0 @@
-/*
-https://www.pythonanywhere.com/user/dummytestnewproject/files/home/dummytestnewproject/DjangoWebProject/DjangoWebProject1/app1
-https://www.pythonanywhere.com/user/dummytestnewproject/files/home/dummytestnewproject/DjangoWebProject/DjangoWebProject1/app1/views.py?edit
-https://www.pythonanywhere.com/user/dummytestnewproject/files/home/dummytestnewproject/DjangoWebProject/DjangoWebProject1/app1/models.py?edit
-https://www.pythonanywhere.com/user/dummytestnewproject/files/home/dummytestnewproject/DjangoWebProject/DjangoWebProject1/app1/urls.py?edit
-
-*/
